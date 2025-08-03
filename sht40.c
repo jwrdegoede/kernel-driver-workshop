e4e2e091cfd8dae69ffbaf01f810830e871eb529
Hans de Goede <hansg@kernel.org>
2025-08-03 17:37:22 +0200

    sht40: Add 'hello' and 'world' sysfs device attributes
    Add example 'hello' and 'world' sysfs device attributes.
    
    Signed-off-by: Hans de Goede <hansg@kernel.org>

--- sht40.c
+++ sht40.c
@@ -5,13 +5,37 @@
  * Copyright (c) 2025 Hans de Goede <hansg@kernel.org>
  */
 
+#include <linux/delay.h>
+#include <linux/device.h>
 #include <linux/i2c.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/sysfs.h>
 
 struct sht40_data {
 	struct i2c_client *client;
 };
+
+static ssize_t hello_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct sht40_data *data = dev_get_drvdata(dev);
+
+	return sysfs_emit(buf, "%s says: 'Hello'\n", dev_name(&data->client->dev));
+}
+static DEVICE_ATTR_RO(hello);
+
+static ssize_t world_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	return sysfs_emit(buf, "Hello World\n");
+}
+static DEVICE_ATTR_RO(world);
+
+static struct attribute *sht40_attrs[] = {
+	&dev_attr_hello.attr,
+	&dev_attr_world.attr,
+	NULL
+};
+ATTRIBUTE_GROUPS(sht40);
 
 static int sht40_probe(struct i2c_client *client)
 {
@@ -37,7 +61,8 @@ MODULE_DEVICE_TABLE(i2c, sht40_i2c_id);
 
 static struct i2c_driver sht40_driver = {
 	.driver = {
-		.name	= "sht40",
+		.name = "sht40",
+		.dev_groups = sht40_groups,
 	},
 	.probe = sht40_probe,
 	.id_table = sht40_i2c_id,
